e8269a229f081132b5d73a4c4cef82d4fbc9d8d1
Artem Martynov <amerta90@mail.ru>
2022-11-20 22:32:40 +0300

    task 2

---
+++ task2/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\t\t\t***Программа определения весов товаров***\n\n\n");
+
+        System.out.println("Введите первый вес: ");
+        double first = new Scanner(System.in).nextDouble();
+
+        System.out.println("Введите второй вес: ");
+        double second = new Scanner(System.in).nextDouble();
+
+        System.out.println("Введите третий вес: ");
+        double third = new Scanner(System.in).nextDouble();
+
+        double greatest = 0;
+        double average = 0;
+        double smallest = 0;
+        //todo дописать логику программы ниже.
+
+
+
+
+
+
+
+        //todo
+
+
+        System.out.println("Наибольший вес: " + 10);
+        System.out.println("Средний вес: " + 9);
+        System.out.println("Наименьший вес: " + 1);
+    }
+}
